5808b7e455eb46fce9c731a64939b3c0ef97f25f
Michal Trubini <mchl.trubini@gmail.com>
2022-07-22 13:21:58 +0200

    hard code mobile WIP

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,11 +1,12 @@
 import mercury from '../assets/images/planet-mercury.svg'
+import '../assets/styles/css/planet.css';
 
 const Planet = () => {
     return ( 
-        <main>
+        <main className='planet'>
             <div className="planet__content-container">
                 <div className="planet__image-container">
-                    <img src={mercury} alt='mercury'/>
+                    <img src={mercury} alt='mercury' className='planet__image'/>
                 </div>
                 <div className="planet__text-container">
                     <div className='planet__text'>
@@ -13,26 +14,26 @@ const Planet = () => {
                         <p className='planet__description'>
                             Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.
                         </p>
-                        <p><span>Source:</span> <a href='something'>Wikipedia</a></p>
+                        <p><span className='planet__source'>Source:</span> <a href='something' className='planet__wikipedia-link'>Wikipedia</a></p>
                     </div>
                 </div>
             </div>
             <div className="planet__stats-container">
                 <div className='planet__stats'>
                     <p className='planet__stats-name'>rotation time</p>
-                    <p className='planet__stats-number'>58.6 days</p>
+                    <p className='planet__stats-number'>58.6<span className='planet__stats-unit'> days</span></p>
                 </div>
                 <div className='planet__stats'>
                     <p className='planet__stats-name'>revolution time</p>
-                    <p className='planet__stats-number'>87.97 days</p>
+                    <p className='planet__stats-number'>87.97<span className='planet__stats-unit'> days</span></p>
                 </div>
                 <div className='planet__stats'>
                     <p className='planet__stats-name'>radius</p>
-                    <p className='planet__stats-number'>2439.7 km</p>
+                    <p className='planet__stats-number'>2439.7<span className='planet__stats-unit'> km</span></p>
                 </div>
                 <div className='planet__stats'>
                     <p className='planet__stats-name'>average temp.</p>
-                    <p className='planet__stats-number'>430°C</p>
+                    <p className='planet__stats-number'>430<span className='planet__stats-unit'>°C</span></p>
                 </div>
             </div>
         </main> 
