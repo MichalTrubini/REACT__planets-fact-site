fea97d8bd74c101b077c4ba05dd9a42379049564
Michal Trubini <mchl.trubini@gmail.com>
2022-07-24 22:14:39 +0200

    tablet css done

--- src/components/InfoNav.js
+++ src/components/InfoNav.js
@@ -1,6 +1,8 @@
 import React, {useContext} from 'react';
 import PlanetContext from '../assets/context/planet-context';
 
+import useWindowDimensions from '../functions/WindowDimensions';
+
 import '../assets/styles/css/infonav.css';
 
 const InfoNav = () => {
@@ -9,23 +11,36 @@ const InfoNav = () => {
     const {section} = useContext(PlanetContext);
     const {planet} = useContext(PlanetContext);
 
+    const {width} = useWindowDimensions();
+
+    const screenBreak = 525;
+
     const classSelector = () => {
-        if (planet === 'Mercury') return 'info-nav__selected-mercury';
-        if (planet === 'Venus') return 'info-nav__selected-venus';
-        if (planet === 'Earth') return 'info-nav__selected-earth';
-        if (planet === 'Mars') return 'info-nav__selected-mars';
-        if (planet === 'Jupiter') return 'info-nav__selected-jupiter';
-        if (planet === 'Saturn') return 'info-nav__selected-saturn';
-        if (planet === 'Uranus') return 'info-nav__selected-uranus';
-        if (planet === 'Neptune') return 'info-nav__selected-neptune';
+        if (planet === 'Mercury' & width < screenBreak) return 'info-nav__selected-mercury-mob';
+        if (planet === 'Venus' & width < screenBreak) return 'info-nav__selected-venus-mob';
+        if (planet === 'Earth' & width < screenBreak) return 'info-nav__selected-earth-mob';
+        if (planet === 'Mars' & width < screenBreak) return 'info-nav__selected-mars-mob';
+        if (planet === 'Jupiter' & width < screenBreak) return 'info-nav__selected-jupiter-mob';
+        if (planet === 'Saturn' & width < screenBreak) return 'info-nav__selected-saturn-mob';
+        if (planet === 'Uranus' & width < screenBreak) return 'info-nav__selected-uranus-mob';
+        if (planet === 'Neptune' & width < screenBreak) return 'info-nav__selected-neptune-mob';
+
+        if (planet === 'Mercury' & width > screenBreak) return 'info-nav__selected-mercury';
+        if (planet === 'Venus' & width > screenBreak) return 'info-nav__selected-venus';
+        if (planet === 'Earth' & width > screenBreak) return 'info-nav__selected-earth';
+        if (planet === 'Mars' & width > screenBreak) return 'info-nav__selected-mars';
+        if (planet === 'Jupiter' & width > screenBreak) return 'info-nav__selected-jupiter';
+        if (planet === 'Saturn' & width > screenBreak) return 'info-nav__selected-saturn';
+        if (planet === 'Uranus' & width > screenBreak) return 'info-nav__selected-uranus';
+        if (planet === 'Neptune' & width > screenBreak) return 'info-nav__selected-neptune';
     }
 
     return (
         <div className='info-nav'>
             <ul className='info-nav__list'>
-                <li id='overview' className={section === 'overview' ? ('info-nav__item info-nav__selected ' + classSelector()) : 'info-nav__item'} onClick={PlanetSectionHandler}>overview</li>
-                <li id='structure' className={section === 'structure' ? ('info-nav__item info-nav__selected ' + classSelector()) : 'info-nav__item'} onClick={PlanetSectionHandler}>structure</li>
-                <li id='geology' className={section === 'geology' ? ('info-nav__item info-nav__selected ' + classSelector()) : 'info-nav__item'} onClick={PlanetSectionHandler}>surface</li>
+                <li id='overview' className={section === 'overview' ? ('info-nav__item info-nav__selected ' + classSelector()) : 'info-nav__item'} onClick={PlanetSectionHandler}><span className='info-nav__section-number'>01</span>overview</li>
+                <li id='structure' className={section === 'structure' ? ('info-nav__item info-nav__selected ' + classSelector()) : 'info-nav__item'} onClick={PlanetSectionHandler}><span className='info-nav__section-number'>02</span>structure</li>
+                <li id='geology' className={section === 'geology' ? ('info-nav__item info-nav__selected ' + classSelector()) : 'info-nav__item'} onClick={PlanetSectionHandler}><span className='info-nav__section-number'>03</span>surface</li>
             </ul>
         </div>
     )
